349f2a2f16bda355101b3ba843709d2cf041ab04
LeoSM_07 <leosmindlin@icloud.com>
2022-10-02 11:38:14 -0700

    Initial Commit

---
+++ HAConnect/HAConnectApp.swift
@@ -0,0 +1,17 @@
+//
+// HAConnectApp.swift
+// HAConnect
+//
+// Created by LeoSM_07 on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct HAConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
